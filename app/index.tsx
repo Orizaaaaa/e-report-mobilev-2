be112f1bf1a9fa98f40e84db0792a01965636911
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-05-10 15:30:21 +0700

    finish  all

--- app/index.tsx
+++ app/index.tsx
@@ -187,6 +187,75 @@ export default function Index() {
         </View>
 
 
+        <View className='flex-row justify-between items-center mb-3 mt-5'>
+          <Text>Laporan reguler</Text>
+          <TouchableOpacity
+            onPress={handlePress}
+          >
+            <Text className='text-blueCustom'>Lihat Semua</Text>
+          </TouchableOpacity>
+
+        </View>
+
+
+        <View style={{ flex: 1 }}>
+          <Carousel
+            ref={ref}
+            width={width * 0.9}
+            height={(width * 0.8) / 1.5}
+            data={dataCaraosel}
+            onProgressChange={progress}
+            style={{
+              width: width,
+              alignSelf: 'flex-start', // penting untuk menempel ke kiri
+            }}
+            mode="parallax"
+            modeConfig={{
+              parallaxScrollingScale: 1,
+              parallaxScrollingOffset: 0,
+              parallaxAdjacentItemScale: 1,
+            }}
+            pagingEnabled={false}
+            loop={false}
+            snapEnabled={false}
+            renderItem={({ item, index }) => (
+              <View
+                key={index}
+                style={{
+                  flex: 1,
+                  marginRight: 10,
+                  justifyContent: 'center',
+
+                }}
+              >
+                <View className='w-full p-3 bg-white rounded-2xl shadow-2xl'>
+                  <View className='flex-row gap-3 space-x-3'>
+                    <Image
+                      className='w-2/5 aspect-square rounded-2xl'
+                      source={item.image}
+                      resizeMode='cover'
+                    />
+
+                    <View className='flex-1'>
+                      <Text className=' text-primary text-base'>{item.title}</Text>
+                      <Text className=' text-primary text-base mt-2'>{item.desc}</Text>
+                    </View>
+                  </View>
+                  <TouchableOpacity
+                    onPress={handlePress}
+                    className='bg-primary p-2 rounded-lg mt-3'
+                  >
+                    <Text className='text-sm text-center text-white'>Selengkapnya</Text>
+                  </TouchableOpacity>
+                </View>
+
+              </View>
+            )}
+          />
+
+        </View>
+
+
       </View>
     </ScrollView >
   );
