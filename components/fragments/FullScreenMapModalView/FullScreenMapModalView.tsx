5618d4f47bfb3e1e66a7690a7b3e2e167ad8161c
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-06-07 10:18:53 +0700

    finish map

--- components/fragments/FullScreenMapModalView/FullScreenMapModalView.tsx
+++ components/fragments/FullScreenMapModalView/FullScreenMapModalView.tsx
@@ -67,14 +67,14 @@ const FullScreenMapModalView: React.FC<FullScreenMapModalViewProps> = ({
                 </MapView>
 
                 <TouchableOpacity
-                    className="absolute bottom-6 right-4 bg-red-500 px-4 py-2 rounded-lg shadow-md"
+                    className="absolute bottom-6 right-4 bg-primaryNavy px-4 py-2 rounded-lg shadow-md"
                     onPress={onClose}
                 >
                     <Text className="text-white text-base font-medium">Tutup Peta</Text>
                 </TouchableOpacity>
                 {selectedLocation && (
                     <TouchableOpacity
-                        className="absolute bottom-6 left-4 bg-green-500 px-4 py-2 rounded-lg shadow-md"
+                        className="absolute bottom-6 left-4 bg-primaryNavy px-4 py-2 rounded-lg shadow-md"
                         onPress={onConfirmLocation}
                     >
                         <Text className="text-white text-base font-medium">Konfirmasi Lokasi Ini</Text>
