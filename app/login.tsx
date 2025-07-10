021cd90222fe30693a20c40f90ef793954407182
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-10 23:26:10 +0700

    finish all

--- app/login.tsx
+++ app/login.tsx
@@ -6,6 +6,7 @@ import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { useRouter } from 'expo-router';
 import {
+    sendPasswordResetEmail,
     signInWithEmailAndPassword
 } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
@@ -13,6 +14,7 @@ import React, { useEffect, useState } from 'react';
 import {
     ActivityIndicator,
     Alert,
+    Modal,
     SafeAreaView,
     Text,
     TouchableOpacity,
@@ -44,6 +46,11 @@ const registerForPushNotificationsAsync = async (): Promise<string | undefined> 
 };
 
 export default function LoginRegisterScreen() {
+    const [modalConfirmVisible, setModalConfirmVisible] = useState(false);
+    const [modalSentVisible, setModalSentVisible] = useState(false);
+    const [emailReset, setEmailReset] = useState({
+        email: '',
+    });
     const [loading, setLoading] = useState(false)
     const [form, setForm] = useState({ email: '', password: '' });
     const handleChange = (name: string, text: string) => {
@@ -122,6 +129,25 @@ export default function LoginRegisterScreen() {
         }
     };
 
+    const handleSendPasswordReset = async () => {
+        try {
+            if (!emailReset.email) {
+                Alert.alert("Gagal", "Email pengguna tidak ditemukan.");
+                return;
+            }
+            await sendPasswordResetEmail(auth, emailReset.email);
+            setEmailReset({ email: '' });
+            setModalConfirmVisible(false);
+            setModalSentVisible(true);
+            router.replace('/login');
+        } catch (error) {
+            console.error("❌ Gagal mengirim email reset password:", error);
+            Alert.alert("Gagal", "Terjadi kesalahan saat mengirim email.");
+        }
+    };
+
+    console.log(emailReset);
+
 
     return (
         <SafeAreaView className='bg-white' style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 20 }}>
@@ -153,6 +179,58 @@ export default function LoginRegisterScreen() {
             <TouchableOpacity className='mt-4' onPress={() => router.push('/register')}>
                 <Text className='text-slate-400 font-light' >Belum punya akun? Register</Text>
             </TouchableOpacity>
+            <TouchableOpacity className='mt-2' onPress={() => setModalConfirmVisible(true)}>
+                <Text className='text-slate-400 font-light' >Lupa kata sandi ?</Text>
+            </TouchableOpacity>
+
+            {/* Modal Konfirmasi Ganti Password */}
+            <Modal
+                transparent
+                visible={modalConfirmVisible}
+                animationType="fade"
+                onRequestClose={() => setModalConfirmVisible(false)}
+            >
+                <View className="flex-1 justify-center items-center bg-black/40 px-4">
+                    <View className="bg-white p-6 rounded-xl w-full">
+                        <Text className="text-lg font-semibold mb-3 text-center">Masukan email anda di bawah ini jika ingin ubah password</Text>
+                        <View className='w-full flex items-center justify-center mb-4' >
+                            <AuthInput placeholder='Email' value={emailReset.email} onChangeText={(text) => setEmailReset({ ...emailReset, email: text })} isPass={false}
+                            />
+                        </View>
+                        <View className="flex-row justify-end gap-4 mt-4">
+                            <TouchableOpacity className='bg-gray-200 py-2 px-4 rounded-lg' onPress={() => setModalConfirmVisible(false)}>
+                                <Text className="text-primaryNavy">Batal</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity className='bg-primaryOrange py-2 px-4 rounded-lg' onPress={handleSendPasswordReset}>
+                                <Text className="text-white font-semibold">Iya</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            {/* Modal Notifikasi Email Terkirim */}
+            <Modal
+                transparent
+                visible={modalSentVisible}
+                animationType="fade"
+                onRequestClose={() => setModalSentVisible(false)}
+            >
+                <View className="flex-1 justify-center items-center bg-black/40 px-4">
+                    <View className="bg-white p-6 rounded-xl w-full">
+                        <Text className="text-lg font-semibold text-center mb-2">Email telah dikirim</Text>
+                        <Text className="text-center text-gray-700">Silakan cek email Anda untuk mengganti password.</Text>
+                        <TouchableOpacity
+                            onPress={() => setModalSentVisible(false)}
+                            className="mt-4 bg-primaryOrange py-2 px-4 rounded-lg self-center"
+                        >
+                            <Text className="text-white font-medium">Tutup</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </SafeAreaView>
+
+
     );
 }
