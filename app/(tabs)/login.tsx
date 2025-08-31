edf13081fda645f7f671db8ea65be70c2f9aaa77
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-08-31 17:08:01 +0700

    finishh

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+type Props = {}
+
+const login = (props: Props) => {
+    return (
+        <View>
+            <Text>login</Text>
+        </View>
+    )
+}
+
+export default login
